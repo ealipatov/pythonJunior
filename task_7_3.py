aa2e4fd13894770cc08813c6c991ee435ca72ab1
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-01 00:28:08 +0300

    Home Work 7. Задача 3

---
+++ task_7_3.py
@@ -0,0 +1,20 @@
+# ДЗ. Отсортировать список от большего к меньшему не используя функцию sort()
+
+from random import randint
+
+numbers = []
+numbers_sorted = []
+counter = 0
+while counter < 10:
+    numbers.append(randint(1, 1000))
+    counter += 1
+print(numbers)
+
+while len(numbers) != 0:
+    max_number = 0
+    for number in numbers:
+        if number > max_number:
+            max_number = number
+    numbers.remove(max_number)
+    numbers_sorted.append(max_number)
+print(numbers_sorted)
